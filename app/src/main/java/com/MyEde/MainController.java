1149f3c6eb67d5c870aad3398292b297570e509d
Cem Başar Ceylani <cembasar07@gmail.com>
2025-04-06 17:46:00 +0300

    Declaring the methods with void type to fill in

--- app/src/main/java/com/MyEde/MainController.java
+++ app/src/main/java/com/MyEde/MainController.java
@@ -1,4 +1,66 @@
 package com.MyEde;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class MainController {
+    @FXML
+    protected void handleNewProject() throws IOException {
+
+    }
+    @FXML
+    protected void handleOpenProject() throws IOException {
+
+    }
+
+    @FXML
+    protected void handleCloseProject() throws IOException {
+
+    }
+
+    @FXML
+    protected void handleEditConfig() throws IOException {
+
+    }
+
+    @FXML
+    protected void handleCreateConfig(){
+
+    }
+    @FXML
+    protected void handleExportConfig(){
+
+    }
+
+    @FXML
+    protected void handleDeleteConfig() {
+
+    }
+
+    @FXML
+    protected void handleCheck() {
+
+    }
+
+    @FXML
+    protected void handleQuit() {
+
+    }
+    @FXML
+    protected void handleAbout() {
+
+    }
+
+    @FXML
+    protected void handleUserManual() {
+
+    }
+
+
 }
