1e7336bf9e1e923ff8f07190a9c781398d89b70f
Cem Başar Ceylani <cembasar07@gmail.com>
2025-04-08 00:18:45 +0300

    Adding fxml files into the com.MyEde and renaming the widgets according to UML diagram

--- app/src/main/java/com/MyEde/WindowController.java
+++ app/src/main/java/com/MyEde/WindowController.java
@@ -1,4 +1,33 @@
 package com.MyEde;
 
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+
 public class WindowController {
+
+    public RadioButton radioCreateMode;
+    public RadioButton radioLoadMode;
+    public VBox vboxImportSection;
+    public VBox vboxCreateSection;
+    public TextField fieldConfigPath;
+    public TextField fieldProjectTitle;
+    public TextField zipFilePath;
+    public ChoiceBox languageChoice;
+    public TextField projectArguments;
+    public TextField projectDestinationPath;
+    public Button configFilePathButton;
+    public Button projectDestinationPathButton;
+    public Button zipFilePathButton;
+    public TextArea expectedOutput;
+    public TextField configFileName;
+    public TextField destinationPath;
+    public Button destinationPathButton;
+    public VBox editConfigVBox;
+    public TextField compCommand;
+    public TextField runCommand;
+    public Button configFilePathEditButton;
+    public TextField configFilePathForDelete;
+    public Button configFilePathDeleteButton;
+
+
 }
